6a274292d09c11a8cda25abdc60418dff44eabb9
kfriendship1003 <kfriendship94@gmail.com>
2020-02-17 18:21:29 +0900

    Practice Velopert Lecture 24

--- index23.js
+++ index23.js
@@ -1,4 +1,4 @@
-//23. 배열함수 - for each
+//23. 배열 내장함수 - for each
 const superheroes = ['ironMan', 'captainAmerica', 'thor', 'doctorStrange']
 
 superheroes.forEach(function (a, b) {
@@ -8,3 +8,45 @@ superheroes.forEach(function (a, b) {
 superheroes.forEach((a, b) => {
   console.log(b, a);
 })
+
+//24. 배열 내장함수 - map
+const list = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squared = list.map(b => b * b);
+console.log(squared);
+
+const asdf = [{
+    id: 1,
+    txts: 'hello'
+  },
+  {
+    id: 2,
+    txts: 'bye'
+  }
+];
+
+const qwer = asdf.map(asdf => asdf.txts);
+console.log(qwer);
+
+//24. 배열 내장함수 - indexOf
+const index = superheroes.indexOf('thor');
+console.log(index);
+
+//24. 배열 내장함수 - findIndex
+const todos = [{
+    name: 1,
+    task: '자바스크립트 입문',
+    done: true
+  },
+  {
+    name: 2,
+    task: '함수 배우기',
+    done: false
+  }
+]
+
+const fi = todos.findIndex(g => g.name === 2);
+console.log(fi);
+
+const find = todos.find(h => h.done === true);
+console.log(find);
