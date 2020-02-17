4f5395427b5986884882a0afdc1a593a7608d98e
kfriendship1003 <kfriendship94@gmail.com>
2020-02-17 15:45:29 +0900

    Practice Velopert Lecture 18

---
+++ index17.js
@@ -0,0 +1,41 @@
+//17. 배열
+const array = [1, 'blabla', 3, 4, 5];
+console.log(array);
+console.log(array[1]);
+
+const objects = [{
+    name: '멍멍이'
+  },
+  {
+    name: '야옹이'
+  }
+];
+
+console.log(objects);
+console.log(objects[1]);
+
+objects.push({
+  name: '멍뭉이'
+});
+
+console.log(objects);
+console.log(objects.length);
+
+//18. 반복문 - for
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+const names = [
+  'bark',
+  'meow',
+  'quack'
+];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i])
+};
