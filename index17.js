6d8c4e4def81b6923d40f4fdc857f61fc8455fb2
kfriendship1003 <kfriendship94@gmail.com>
2020-02-17 17:15:38 +0900

    Practice Velopert Lecture 22

--- index17.js
+++ index17.js
@@ -1,13 +1,13 @@
 //17. 배열
-const array = [1, 'blabla', 3, 4, 5];
+const array = [1, "blabla", 3, 4, 5];
 console.log(array);
 console.log(array[1]);
 
 const objects = [{
-    name: '멍멍이'
+    name: "멍멍이"
   },
   {
-    name: '야옹이'
+    name: "야옹이"
   }
 ];
 
@@ -15,7 +15,7 @@ console.log(objects);
 console.log(objects[1]);
 
 objects.push({
-  name: '멍뭉이'
+  name: "멍뭉이"
 });
 
 console.log(objects);
@@ -30,15 +30,11 @@ for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-const names = [
-  'bark',
-  'meow',
-  'quack'
-];
+const names = ["bark", "meow", "quack"];
 
 for (let i = 0; i < names.length; i++) {
-  console.log(names[i])
-};
+  console.log(names[i]);
+}
 
 //19. 반복문 - while
 let i = 0;
@@ -66,15 +62,15 @@ for (let number of numerics) {
   console.log(number);
 }
 
-const texts = ['tlq', 'dltlq', 'tkatlq', 'tktlq', 'dhtlq'];
+const texts = ["tlq", "dltlq", "tkatlq", "tktlq", "dhtlq"];
 
 for (let text of texts) {
   console.log(text);
 }
 
 const bori = {
-  name: '보리',
-  sound: '엉뫄',
+  name: "보리",
+  sound: "엉뫄",
   age: 1
 };
 
@@ -92,3 +88,29 @@ for (let x = 0; x < 10; x++) {
   console.log(x);
   if (x === 5) break;
 }
+
+//22. 반복문 - 연습
+function sumOf(nums) {
+  let sum = 0;
+  for (let y = 0; y < nums.length; y++) {
+    sum += nums[y];
+  }
+  return sum;
+}
+
+const finish = sumOf([1, 2, 3, 4, 5]);
+console.log(finish);
+
+//22. 반복문 - quiz
+function biggerThanThree(picks) {
+  const array = [];
+  for (let z = 0; z < picks.length; z++) {
+    if (picks[z] > 3) {
+      array.push(picks[z]);
+    }
+  }
+  return array;
+}
+
+const picks = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(picks)); // [4, 5, 6, 7]
