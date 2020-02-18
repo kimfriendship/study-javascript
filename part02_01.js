effa0bd4634e49c84ba34964ce077d2ac1933ce2
kfriendship1003 <kfriendship94@gmail.com>
2020-02-18 13:38:43 +0900

    Practice Velopert Lecture02

--- part02_01.js
+++ part02_01.js
@@ -24,3 +24,39 @@ const condition2 = false;
 const value = condition1 ? '와우!' : condition2 ? 'blabla' : 'foo'
 
 console.log(value);
+
+//02. falsy
+console.log(!undefined)
+console.log(!null)
+console.log(!0)
+console.log(!'')
+console.log(!NaN)
+console.log(!false)
+
+const value1 = null;
+
+if (value1) {
+  console.log('value가 truthy 하네요')
+}
+
+const falsy = !!value1
+console.log(falsy)
+
+//02. truthy
+console.log(!3)
+console.log(!'hello')
+console.log(!['array'])
+console.log(!{})
+
+const value2 = {
+  a: 1
+};
+
+if (value2) {
+  console.log('value가 truthy 하네요')
+};
+
+const truthy = value2 ? true : false;
+console.log(truthy);
+
+//03. 단축 평가 논리 계산법
