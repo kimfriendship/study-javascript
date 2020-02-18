432213cdcefca0adfb4eb2149675e38a949edf5a
kfriendship1003 <kfriendship94@gmail.com>
2020-02-18 11:42:25 +0900

    Finish Part01

---
+++ part01_31.js
@@ -0,0 +1,17 @@
+function Animal(type, name, sound) {
+  this.type = type;
+  this.name = name;
+  this.sound = sound;
+}
+
+Animal.prototype.say = function () {
+  console.log(this.sound)
+}
+
+Animal.prototype.sharedValue = 1;
+
+const puppy = new Animal('개', '멍멍이', '멍멍');
+const kitten = new Animal('고양이', '야옹이', '야옹');
+
+dog.sharedValue
+cat.sharedValue
