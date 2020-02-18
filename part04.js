a3676dc8d4a27cf8bd73379aa47257280ee5aff0
kfriendship1003 <kfriendship94@gmail.com>
2020-02-18 18:20:04 +0900

    Practice Velopert Lecture 02

--- part04.js
+++ part04.js
@@ -1,3 +1,5 @@
+//01. 카운터 만들기
+
 const number = document.getElementById("number");
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
@@ -10,4 +12,19 @@ increase.onclick = () => {
 decrease.onclick = () => {
   const current = parseInt(number.innerText, 10);
   number.innerText = current - 1;
+};
+
+
+
+//02. 모달 만들기
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.querySelector('.modal-wrapper');
+
+open.onclick = () => {
+  modal.style.display = "flex";
+};
+
+close.onclick = () => {
+  modal.style.display = 'none';
 };
