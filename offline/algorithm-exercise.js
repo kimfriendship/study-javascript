ffd2c72c537321e09873d6b81bb7a945e240e8c3
kfriendship1003 <kfriendship94@gmail.com>
2020-03-16 02:02:57 +0900

    binary search practice

--- offline/algorithm-exercise.js
+++ offline/algorithm-exercise.js
@@ -12,16 +12,27 @@ console.log(linearSearch([1, 2, 3, 4, 5, 6], 1));
 // 2. 이진 검색
 function binarySearch(array, target) {
   let result = -1;
-  let i = Math.floor(array.length / 2);
-  while (array[i] !== target) {
-    array[i] > target ? i-- : i++;
-    if (array[i] === target) result = i;
-    if (!(i >= 0 && i < array.length)) break;
+  let start = 0;
+  let end = array.length - 1;
+  let mid = Math.floor(start + end / 2);
+  while (true) {
+    if (start === end && array[mid] !== target) {
+      break;
+    } else if (array[mid] > target) {
+      end = mid - 1;
+      mid = Math.floor(start + end / 2);
+    } else if (array[mid] < target) {
+      start = mid + 1;
+      mid = Math.floor((end - start) / 2) + start;
+    } else if (array[mid] === target) {
+      result = mid;
+      break;
+    }
   }
   return result;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 0));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9));
 
 // 3. 버블 정렬
 function bubbleSort(array) {
