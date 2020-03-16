ce6645c8e37a84125c0821d6f9b1616ecf124f88
kfriendship1003 <kfriendship94@gmail.com>
2020-03-16 09:43:54 +0900

    finish algorithm practice

--- offline/algorithm-exercise.js
+++ offline/algorithm-exercise.js
@@ -7,7 +7,10 @@ function linearSearch(array, target) {
   return result;
 }
 
-console.log(linearSearch([1, 2, 3, 4, 5, 6], 1));
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
 
 // 2. 이진 검색
 function binarySearch(array, target) {
@@ -18,40 +21,43 @@ function binarySearch(array, target) {
   while (true) {
     if (start === end && array[mid] !== target) {
       break;
+    } else if (array[mid] === target) {
+      result = mid;
+      break;
     } else if (array[mid] > target) {
       end = mid - 1;
       mid = Math.floor(start + end / 2);
     } else if (array[mid] < target) {
       start = mid + 1;
       mid = Math.floor((end - start) / 2) + start;
-    } else if (array[mid] === target) {
-      result = mid;
-      break;
     }
   }
   return result;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9));
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)); // 8
 
 // 3. 버블 정렬
 function bubbleSort(array) {
-  let newArray = [array[0]];
-  for (let i = 1; i < array.length - 1; i++) {
+  let sortArray = [array[0]];
+  for (let j = 1; j < array.length - 1; j++) {
     for (let i = 1; i < array.length; i++) {
-      if (newArray[i - 1] < array[i]) {
-        newArray[i] = array[i];
+      if (sortArray[i - 1] < array[i]) {
+        sortArray[i] = array[i];
       } else {
-        newArray[i] = newArray[i - 1];
-        newArray[i - 1] = array[i];
+        sortArray[i] = sortArray[i - 1];
+        sortArray[i - 1] = array[i];
       }
     }
-    array = newArray;
-    newArray = [array[0]];
+    array = sortArray;
+    sortArray = [array[0]];
   }
   return array;
 }
 
-console.log(bubbleSort([2, 4, 5, 1, 3]));
-console.log(bubbleSort([5, 2, 1, 3, 4, 6]));
-console.log(bubbleSort([3, 1, 0, -1, 4, 2]));
+console.log(bubbleSort([2, 4, 5, 1, 3])); // [ 1, 2, 3, 4, 5 ]
+console.log(bubbleSort([5, 2, 1, 3, 4, 6])); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [ -1, 0, 1, 2, 3, 4 ]
