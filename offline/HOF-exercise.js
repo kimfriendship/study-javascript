7fea66fc285f0a2f3d141c8285c92bd8e6fcca01
kfriendship1003 <kfriendship94@gmail.com>
2020-03-23 16:44:24 +0900

    practice 02

--- offline/HOF-exercise.js
+++ offline/HOF-exercise.js
@@ -15,7 +15,7 @@ const todos = [{
 
 function render() {
   let html = '';
-  todos.forEach((todo) => {
+  todos.forEach(todo => {
     html += `<li id="${todo.id}">
   <label><input type="checkbox"${todo.completed ? ' checked' : ''}>${todo.content}</label>
 </li>
@@ -25,3 +25,14 @@ function render() {
 }
 
 console.log(render());
+
+// 2. 특정 프로퍼티 값 추출
+function getValues(key) {
+  return todos.map(todo => todo[key]);
+}
+
+console.log(getValues('id'));
+console.log(getValues('content'));
+console.log(getValues('completed'));
+
+// 3. 
