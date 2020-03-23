3d2514930280eb66e8455126cd12ee3ba026830a
kfriendship1003 <kfriendship94@gmail.com>
2020-03-23 21:08:00 +0900

    practice again on my own

--- offline/HOF-exercise.js
+++ offline/HOF-exercise.js
@@ -8,16 +8,16 @@ let todos = [{
   completed: true
 }, {
   id: 1,
-  content: 'Javascript',
+  content: 'JavaScript',
   completed: false
-}];
+}]
 
 // 1. html 생성
 function render() {
   let html = '';
   todos.forEach(todo => {
     html += `<li id="${todo.id}">
-  <label><input type="checkbox"${todo.completed ? ' checked' : ''}>${todo.content}</label>
+  <label><input type="checkbox"${todo.completed? ' checked' : ''}>${todo.content}</label>
 </li>
 `
   })
@@ -36,30 +36,67 @@ console.log(getValues('content'));
 console.log(getValues('completed'));
 
 // 3. 프로퍼티 정렬
+function sortBy(key) {
 
-// 4. 새로운 요소 추가
-function addTodo(newTodo) {
-  todos = [todos, ...[newTodo]].flat();
 }
 
-addTodo({
+console.log(sortBy('id'));
+console.log(sortBy('content'));
+console.log(sortBy('completed'));
+
+
+// 4. 새로운 요소 추가
+function addTodo1(newTodo) {
+  todos = [...[newTodo], ...todos].flat();
+}
+
+function addTodo2(newTodo) {
+  todos = [newTodo].concat(todos);
+}
+
+addTodo1({
   id: 4,
   content: 'Test',
   completed: true
 });
 console.log(todos);
 
+addTodo2({
+  id: 5,
+  content: 'React',
+  completed: false
+});
+console.log(todos);
+
 // 5. 특정 요소 삭제
 function removeTodo(id) {
-  todos = todos.filter(todo => todo.id !== id);
+  todos = todos.filter(todo => todo.id !== id)
 }
 
 removeTodo(2);
 console.log(todos);
 
+// 6. 특정 요소의 프로퍼티 값 반전
+function toggleCompletedById(id) {
+  todos.map(todo => {
+    if (todo.id === id) todo.completed = !(todo.completed);
+  })
+}
+
+toggleCompletedById(3);
+console.log(todos);
+
+// 7. 모든 요소의 completd 프로퍼티 값을 true로 설정
+function toggleCompletedAll() {
+
+}
+
+toggleCompletedAll();
+console.log(todos);
+
 // 8. completed 프로퍼티의 값이 true인 요소의 갯수 구하기
 function countCompletedTodos() {
-  const result = todos.filter(todo => todo.completed).length;
+  const result = todos.filter(todo => todo.completed).length
   return result;
 }
 
@@ -67,7 +104,7 @@ console.log(countCompletedTodos());
 
 // 9. id 프로퍼티의 값 중에서 최대값 구하기
 function getMaxId() {
-  return todos.length ? Math.max(...todos.map(todo => todo.id)) : 0;
+  return Math.max(...todos.map(todo => todo.id));
 }
 
 console.log(getMaxId());
