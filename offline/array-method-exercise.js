60f190ef63fbfd3ec5681a46fb359afaf0ff3840
kfriendship1003 <kfriendship94@gmail.com>
2020-03-24 21:31:10 +0900

    practice array methods

--- offline/array-method-exercise.js
+++ offline/array-method-exercise.js
@@ -1,4 +1,4 @@
-// 1. Array.isArray
+// 1. Array.isArray: returns boolean
 console.log(Array.isArray([])); // true
 console.log(Array.isArray(new Array)); // true
 console.log(Array.isArray({})); // false
@@ -7,7 +7,7 @@ console.log(Array.isArray({
   length: 1
 })); // false
 
-// 2. Array.prototype.indexOf
+// 2. Array.prototype.indexOf: returns index number
 const arr = [1, 2, 1, 4];
 
 console.log(arr.indexOf(2)); // 1
@@ -16,7 +16,7 @@ console.log(arr.indexOf(1, 1)); // 2
 if (!arr.includes(3)) arr.push(3);
 console.log(arr); // [ 1, 2, 1, 4, 3 ]
 
-// 3. Array.prototype.push (mutator)
+// 3. Array.prototype.push (mutator): returns length of modified array
 console.log(arr.push(5)); // 6
 
 arr[arr.length] = 6;
@@ -25,21 +25,83 @@ console.log(arr); // [1, 2, 1, 4, 3, 5, 6]
 let newArr = [...arr, 7];
 console.log(newArr); // [1, 2, 1, 4, 3, 5, 6, 7]
 
-// 4. Array.prototype.pop (mutator)
+// 4. Array.prototype.pop (mutator): returns removed elements
 console.log(arr.pop()); // 6
 console.log(arr); // [ 1, 2, 1, 4, 3, 5 ]
 
-// 5. Array.prototype.unshift (mutator)
+// 5. Array.prototype.unshift (mutator): returns length of modified array
 arr.unshift(-1, 0);
 console.log(arr); // [-1, 0, 1, 2, 1, 4, 3, 5]
 
 newArr = [-2, ...arr];
 console.log(newArr); // [-2, -1, 0, 1, 2, 1,  4, 3, 5]
 
-// 6. Array.prototype.shift (mutator)
+// 6. Array.prototype.shift (mutator): returns removed elements
 arr.shift();
 console.log(arr); // [0, 1, 2, 1, 4, 3, 5]
 newArr.shift(5);
 console.log(newArr); // [-1, 0, 1, 2, 1,  4, 3, 5]
 
-// 7. Array.prototype.concat
+// 7. Array.prototype.concat (accessor): returns added new array
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+let result = arr1.concat(arr2);
+console.log(result); // [1, 2, 3, 4, 5, 6]
+console.log(arr1); // [1, 2, 3]
+
+// 8. Array.porototype.splice (mutator): returns removed elements
+let splice = result.splice(1, 2, 20, 30);
+
+console.log(splice); // [2, 3]
+console.log(result); // [1, 20, 30, 4, 5, 6]
+
+splice = result.splice(1, 0, 15);
+
+console.log(splice); // []
+console.log(result); // [1, 15, 20, 30, 4, 5, 6]
+
+// 9. Array.prototype.slice (accessor): returns copied array
+let slice = result.slice(2, 4);
+
+console.log(slice); // [20, 30]
+console.log(result); // [1, 15, 20, 30, 4, 5, 6]
+
+slice = result.slice();
+
+console.log(slice); // [1, 15, 20, 30, 4, 5, 6]
+console.log(result); // [1, 15, 20, 30, 4, 5, 6]
+
+// 10. Array.prototype.join (accessor): returns joined array
+result = slice.join('*');
+
+console.log(result); // 1*15*20*30*4*5*6
+
+// 11. Array.prototype.reverse (mutator): returns modified array
+result = slice.reverse();
+
+console.log(result); // [6, 5, 4, 30, 20, 15, 1]
+console.log(slice); // [6, 5, 4, 30, 20, 15, 1]
+
+// 12. Array.prototype.fill (mutator): returns modified array
+result = slice.fill(0, 3, 6);
+
+console.log(result); // [6, 5, 4, 0, 0, 0, 1]
+console.log(slice); // [6, 5, 4, 0, 0, 0, 1]
+
+// 13. Array.prototype.includes: returns boolean
+result = arr.includes(3);
+
+console.log(result); // true
+
+result = slice.includes(0, -3);
+
+console.log(result); // true
+
+//14. Array.prototype.flat
+const unflat = [1, [2, 3, [4, 5, [6]]]];
+
+console.log(unflat.flat()); // [ 1, 2, 3, [ 4, 5, [ 6 ] ] ]
+console.log(unflat.flat(2)); // [ 1, 2, 3, 4, 5, [ 6 ] ]
+console.log(unflat.flat(Infinity)); // [ 1, 2, 3, 4, 5, 6 ]
+
