c4de4c118e1d4f6df90d4b489f4bded082bc80c4
kimfriendship <kfriendship94@gmail.com>
2021-01-21 01:17:16 +0900

    :sparkles: Learn Caching, chunk, minification ...

--- webpack/browser/webpack.config.js
+++ webpack/browser/webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].[chunkhash].js", //hash, contenthash, chunkhash
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -12,11 +16,14 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [
+          // {
+          //   loader: "style-loader",
+          //   options: {
+          //     injectType: "singletonStyleTag",
+          //   },
+          // },
           {
-            loader: "style-loader",
-            options: {
-              injectType: "singletonStyleTag",
-            },
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
@@ -26,12 +33,53 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./template.html",
+      title: "Webpack",
+      template: "./template.hbs",
+      meta: {
+        viewport: "width=device-width, initial-scale=1.0",
+      },
+      minify: {
+        collapseWhitespace: true,
+        useShortDoctype: true,
+        removeScriptTypeAttributes: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].css",
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default", { discardComments: { removeAll: true } }],
+      },
+      canPrint: true,
     }),
   ],
+  optimization: {
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "venders",
+          chunks: "all",
+        },
+      },
+    },
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()],
+  },
   mode: "none",
 };
