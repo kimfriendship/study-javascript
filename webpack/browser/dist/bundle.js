574cd4e4b7cddfd34ddd114531d30671c754a256
kimfriendship <kfriendship94@gmail.com>
2021-01-20 15:29:37 +0900

    :sparkles: Try using webpack on browser

---
+++ webpack/browser/dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "Hello Webpack";
+  return element;
+}
+
+document.body.appendChild(component());
+
+/******/ })()
+;
